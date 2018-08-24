d67600c044ed04ff0390b51ad3631cca33c6c9ef
gcmurillo <gcmurillo@outlook.com>
2018-08-24 11:11:50 -0500

    visualization

--- stackVisualization/src/pages/home/home.ts
+++ stackVisualization/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RestProvider } from '../../providers/rest/rest';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  data: any;
 
+  constructor(public navCtrl: NavController, public restProvider: RestProvider) {
+      this.getData();
+  }
+
+  getData() {
+    this.restProvider.getData()
+      .then(data => {
+        this.data = data;
+        console.log(this.data);
+      });
   }
 
 }
---
+++ stackVisualization/src/providers/rest/rest.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the RestProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RestProvider {
+  apiUrl = 'assets/data2.json';
+
+  constructor(public http: HttpClient) {
+    console.log('Hello RestProvider Provider');
+  }
+
+  getData() {
+    return new Promise(resolve => {
+      this.http.get(this.apiUrl).subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+}
